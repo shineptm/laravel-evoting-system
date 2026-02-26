1db150bfa76a54a762d83e6dec87e13705c09deb
shine-euphontec <shine@euphontec.com>
2026-02-26 23:26:05 +0530

    Changed close icons

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -15,16 +15,7 @@
 
     <button onclick="this.parentElement.remove()"
         class="absolute top-2 right-2 text-red-700 hover:text-red-900 font-bold">
-       <svg xmlns="http://www.w3.org/2000/svg"
-         class="h-4 w-4"
-         fill="none"
-         viewBox="0 0 24 24"
-         stroke="currentColor">
-        <path stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18L18 6M6 6l12 12" />
-        </svg>
+        X
     </button>
 </div>
 @endif
@@ -50,16 +41,7 @@
 
     <button onclick="this.parentElement.remove()"
         class="absolute top-2 right-2 text-red-700 hover:text-red-900 font-bold">
-        <svg xmlns="http://www.w3.org/2000/svg"
-         class="h-4 w-4"
-         fill="none"
-         viewBox="0 0 24 24"
-         stroke="currentColor">
-        <path stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18L18 6M6 6l12 12" />
-    </svg>
+         X
     </button>
 </div>
 @endif
